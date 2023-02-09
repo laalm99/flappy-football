c125cf4110f22842a4a1c20f9cefba07773bea99
laalm99 <95426832+laalm99@users.noreply.github.com>
2023-02-09 10:29:22 +0300

    Canvas UI Edits

---
+++ flappy-football/Assets/Scripts/Ground.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+       // Vector3 targetScale = ;
+    }
+
+}
